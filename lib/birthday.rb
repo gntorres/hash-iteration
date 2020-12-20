ce94df9d9e467183656191ea280eceb373f3d413
gntorres <gntorres421@gmail.com>
2020-12-20 13:10:36 -0600

    commit tree 997e22cc663bcb1c9a513a419ffbdf19b2061138

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,9 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |person, age|
+    puts "Happy Birthday #{person}! You are now #{age} years old!"
+  end
 end
 
 
